25e8f24e0a6ab5d7f39a8cda72936665c69a04d1
Jama <github@jmstfv.com>
2023-09-14 15:31:43 +0400

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,44 @@
+import json
+import datetime
+import os
+import re
+
+def json_to_markdown(data):
+    title = data.get("title")
+    create_time = datetime.datetime.fromtimestamp(data.get("create_time")).strftime('%Y-%m-%d %H:%M:%S')
+    update_time = datetime.datetime.fromtimestamp(data.get("update_time")).strftime('%Y-%m-%d %H:%M:%S')
+
+    markdown_str = f"# {title}\n\n"
+    markdown_str += f"Created on: {create_time}\n\n"
+    if update_time is not None:
+        markdown_str += f"Updated on: {update_time}\n\n"
+
+    for key, value in data["mapping"].items():
+        message_obj = value.get("message")
+        if message_obj:
+            role = message_obj["author"]["role"]
+            content = message_obj["content"].get("parts", [""])[0] if "parts" in message_obj["content"] else ""
+            if role == "user":
+                markdown_str += f"**User**: {content}\n\n"
+            elif role == "assistant":
+                markdown_str += f"**Assistant**: {content}\n\n"
+
+    return markdown_str
+
+def title_to_filename(title):
+    filename = re.sub(r'[^a-z0-9_]', '', title.lower().replace(' ', '_'))
+    return f"{filename}.md"
+
+# CHANGE THIS
+full_path_to_conversations = "conversations.json"
+
+with open(full_path_to_conversations, 'r') as f:
+    data = json.load(f)
+
+for item in data:
+    markdown_content = json_to_markdown(item)
+    file_name = title_to_filename(item['title'])
+    with open(os.path.join("/tmp/conversations", file_name), 'w') as md_file:
+        md_file.write(markdown_content)
+
+print("Conversations saved to /tmp/conversations directory!")
