f828d96dc368183c04b3dcd786ab19c4f9352a6c
Jama <github@jmstfv.com>
2024-01-18 14:43:46 +0400

    Create folder for markdown files
    Standardize strings, add variable

--- convert.py
+++ convert.py
@@ -5,8 +5,8 @@ import re
 
 def json_to_markdown(data):
     title = data.get("title")
-    create_time = datetime.datetime.fromtimestamp(data.get("create_time")).strftime('%Y-%m-%d %H:%M:%S')
-    update_time = datetime.datetime.fromtimestamp(data.get("update_time")).strftime('%Y-%m-%d %H:%M:%S')
+    create_time = datetime.datetime.fromtimestamp(data.get("create_time")).strftime("%Y-%m-%d %H:%M:%S")
+    update_time = datetime.datetime.fromtimestamp(data.get("update_time")).strftime("%Y-%m-%d %H:%M:%S")
 
     markdown_str = f"# {title}\n\n"
     markdown_str += f"Created on: {create_time}\n\n"
@@ -26,19 +26,22 @@ def json_to_markdown(data):
     return markdown_str
 
 def title_to_filename(title):
-    filename = re.sub(r'[^a-z0-9_]', '', title.lower().replace(' ', '_'))
+    filename = re.sub(r"[^a-z0-9_]", "", title.lower().replace(" ", "_"))
     return f"{filename}.md"
 
-# CHANGE THIS
+# CHANGE THIS!
 full_path_to_conversations = "conversations.json"
+full_path_to_markdown = "/tmp/conversations"
 
-with open(full_path_to_conversations, 'r') as f:
+os.makedirs(full_path_to_markdown)
+
+with open(full_path_to_conversations, "r") as f:
     data = json.load(f)
 
 for item in data:
     markdown_content = json_to_markdown(item)
-    file_name = title_to_filename(item['title'])
-    with open(os.path.join("/tmp/conversations", file_name), 'w') as md_file:
+    file_name = title_to_filename(item["title"])
+    with open(os.path.join(full_path_to_markdown, file_name), "w") as md_file:
         md_file.write(markdown_content)
 
-print("Conversations saved to /tmp/conversations directory!")
+print(f"Conversations saved to {full_path_to_markdown} directory!")
